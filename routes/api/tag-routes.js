7f66f984bda312f0865a2958d9f5fe32d215856f
Baljot Hansi <baljotshansi@gmail.com>
2023-11-17 14:03:40 -0400

    Implemented logic for /api/tags/:id PUT and DELETE routes.

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -75,11 +75,56 @@ router.post('/', async (req, res) => {
 });
 
 router.put('/:id', async (req, res) => {
-  // update a tag's name by its `id` value
+  try {
+    const tagData = await Tag.findByPk(req.params.id);
+
+    if(!tagData) {
+      res.status(404).json({
+        message: 'Requested tag not found.'
+      });
+      return;
+    }
+
+    tagData.tag_name = req.body.tag_name;
+    await tagData.save();
+
+    res.status(200).json({
+      message: 'Successfully updated tag name.'
+    });
+  }
+  catch(err) {
+    res.status(400).json(err);
+  }
 });
 
 router.delete('/:id', async (req, res) => {
-  // delete on tag by its `id` value
+  try {
+    const deletedTag = await Tag.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if(!deletedTag) {
+      res.status(404).json({
+        message: 'Requested tag not found.'
+      });
+      return;
+    }
+
+    await ProductTag.destroy({
+      where: {
+        tag_id: req.params.id
+      }
+    });
+
+    res.status(200).json({
+      message: 'Successfully deleted tag.'
+    });
+  }
+  catch(err) {
+    res.status(400).json(err);
+  }
 });
 
 module.exports = router;
